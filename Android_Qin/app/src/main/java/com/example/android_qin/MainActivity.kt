6ded04b009b9c1f4c6b538e244d4d9d6986cbd07
a1020151695 <1020151695@qq.com>
2020-12-17 13:35:25 +0800

    gaode api ok

--- Android_Qin/app/src/main/java/com/example/android_qin/MainActivity.kt
+++ Android_Qin/app/src/main/java/com/example/android_qin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.android_qin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amap.api.maps2d.AMap
+import com.amap.api.maps2d.MapView
+import com.amap.api.maps2d.model.MyLocationStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var mMapView = findViewById<MapView>(R.id.map)
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        var aMap: AMap?=null
+        if (aMap == null) {
+            aMap = mMapView.map
+        }
+        var myLocationStyle: MyLocationStyle = MyLocationStyle() //初始化定位蓝点样式类//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000) //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.showMyLocation(true)
+        aMap!!.setMyLocationStyle(myLocationStyle) //设置定位蓝点的Style
+        aMap.uiSettings.isMyLocationButtonEnabled = true//设置默认定位按钮是否显示，非必需设置。
+        aMap!!.isMyLocationEnabled = true // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) //连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+        mMapView.onCreate(savedInstanceState)
+
     }
 }

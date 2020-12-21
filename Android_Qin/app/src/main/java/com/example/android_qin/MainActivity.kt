19b81621163cdba7391c98ae90974eaff8fd44dd
a1020151695 <1020151695@qq.com>
2020-12-21 17:15:25 +0800

    get json ok

--- Android_Qin/app/src/main/java/com/example/android_qin/MainActivity.kt
+++ Android_Qin/app/src/main/java/com/example/android_qin/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.android_qin
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.amap.api.maps2d.AMap
 import com.amap.api.maps2d.MapView
 import com.amap.api.maps2d.model.MyLocationStyle
 import com.xuexiang.xui.XUI
+import com.xuexiang.xui.widget.edittext.ClearEditText
+import com.xuexiang.xui.widget.edittext.PasswordEditText
 import org.json.JSONArray
+import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -18,14 +25,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         XUI.init(this.application) // 初始化UI框架
         XUI.debug(true)  // 开启UI框架调试日志
-        checkLocalAccount()
+        grantPermission()
+        //checkLocalAccount()
         setContentView(R.layout.activity_main)
         configLoginBtn()
     }
     private fun checkLocalAccount(){
         // valid day 5
         // "username":"","password":"","role":"","loginDate":""
-        val intent = Intent(this, TeacherActivity::class.java).apply {}
+        val intent = Intent(this, StudentActivity::class.java).apply {}
         startActivity(intent)
 //        val intent = Intent(this, TeacherActivity::class.java).apply {}
 //        startActivity(intent)
@@ -56,27 +64,51 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
     private fun login(): Int {
-//        Thread {
-//            val url = URL("https://api.thecatapi.com/v1/images/search")
-//            val connection = url.openConnection() as HttpURLConnection
-//            connection.requestMethod = "GET"
-//            val inputstream = connection.inputStream
-//            val reader = inputstream.bufferedReader()
-//            var response = StringBuilder()
-//            while (true)
-//            {
-//                val line = reader.readLine() ?: break
-//                response.append(line)
-//            }
-//            reader.close()
-//            connection.disconnect()
-//            val jsonsString = response.toString()
-//            val jsons = JSONArray(jsonsString)
-//            Log.i("jsons",jsonsString)
-//            runOnUiThread{
-//
-//            }
-//        }.start()
+        val userName = findViewById<ClearEditText>(R.id.user_name)
+        val password = findViewById<PasswordEditText>(R.id.password)
+        Thread {
+            val url = URL("http://10.60.0.13:8080/student/login?name="+userName.text+"&password="+password.text)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            val inputstream = connection.inputStream
+            val reader = inputstream.bufferedReader()
+            var response = StringBuilder()
+            while (true)
+            {
+                val line = reader.readLine() ?: break
+                response.append(line)
+            }
+            reader.close()
+            connection.disconnect()
+            val jsonsString = response.toString()
+            val jsons = JSONObject(jsonsString)
+            Log.i("json",jsonsString)
+            runOnUiThread{
+
+            }
+        }.start()
         return 1
+    }
+    fun grantPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this,"granting permission",Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                            , Manifest.permission.ACCESS_FINE_LOCATION
+                            , Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_NETWORK_STATE,
+                            Manifest.permission.INTERNET,
+                            Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS
+                    ),
+                    10)
+        }
+        else{
+            Toast.makeText(this,"already get",Toast.LENGTH_SHORT).show()
+        }
     }
 }

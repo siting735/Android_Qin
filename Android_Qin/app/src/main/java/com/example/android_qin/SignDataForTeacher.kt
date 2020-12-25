cc00adfb4776242b28236c1cbd67f55853b21d8a
a1020151695 <1020151695@qq.com>
2020-12-25 13:23:16 +0800

    add icon in sign data page for teacher

--- Android_Qin/app/src/main/java/com/example/android_qin/SignDataForTeacher.kt
+++ Android_Qin/app/src/main/java/com/example/android_qin/SignDataForTeacher.kt
@@ -33,63 +33,75 @@ class SignDataForTeacher : Fragment() {
         getClassListForTeacher()
         configSwipeRefresh()
     }
-    private fun getClassListForTeacher(){
-        val classesInfoString=arguments?.get("classesInfoString").toString()
+
+    private fun getClassListForTeacher() {
+        val classesInfoString = arguments?.get("classesInfoString").toString()
         val classesInfo = JSONArray(classesInfoString)
-        Thread{
-            for(index in 0 until classesInfo.length()){
+        Thread {
+            for (index in 0 until classesInfo.length()) {
                 addClassInfoToLayout(classesInfo[index] as JSONObject)
             }
         }.start()
     }
-    private fun configSwipeRefresh(){
-        val swipe= view?.findViewById<SwipeRefreshLayout>(R.id.sign_data_swipe_for_teacher)
+
+    private fun configSwipeRefresh() {
+        val swipe = view?.findViewById<SwipeRefreshLayout>(R.id.sign_data_swipe_for_teacher)
         swipe?.setOnRefreshListener {
             removeOriginClassInfos()
             getClassInfosForTeacher()
-            swipe.isRefreshing=false
+            swipe.isRefreshing = false
         }
     }
-    private fun removeOriginClassInfos(){
+
+    private fun removeOriginClassInfos() {
 
     }
-    private fun getClassInfosForTeacher(){
+
+    private fun getClassInfosForTeacher() {
         //teacherId
         //teacher/teacherClasses?teacherId=2001
     }
-    private fun buildBundleForClassInfo(classId:String?,className:String?):Bundle{
+
+    private fun buildBundleForClassInfo(classId: String?, className: String?): Bundle {
         val bundle = Bundle()
-        bundle.putString("classId",classId)
-        bundle.putString("className",className)
+        bundle.putString("classId", classId)
+        bundle.putString("className", className)
         return bundle
     }
-    private fun addClassInfoToLayout(classInfo:JSONObject){
-        val classListLayout= view?.findViewById<LinearLayout>(R.id.class_list)
+
+    private fun addClassInfoToLayout(classInfo: JSONObject) {
+        val classListLayout = view?.findViewById<LinearLayout>(R.id.class_list)
         val classInfoView = SuperTextView(context)
         val className = classInfo?.get("className")?.toString()
         val classId = classInfo?.get("classId")?.toString()
-        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_for_teacher) as NavHostFragment
+        val navHostFragment =
+            activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_for_teacher) as NavHostFragment
         val navController = navHostFragment.navController
+        classInfoView.setLeftIcon(R.drawable.class_icon)
         classInfoView.setLeftString(className)
         classInfoView.useShape()
         classInfoView.setShapeCornersRadius(dip2px(10f).toFloat())
-        var layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dip2px(60f))
-        layoutParams.setMargins(dip2px(10f),dip2px(10f),dip2px(10f),0)
+        classInfoView.setPadding(dip2px(14f), 0, 0, 0)
+        var layoutParams =
+            LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dip2px(60f))
+        layoutParams.setMargins(dip2px(10f), dip2px(10f), dip2px(10f), 0)
         classInfoView.layoutParams = layoutParams
         classInfoView.setRightIcon(R.drawable.get_in)
         classInfoView.setOnClickListener {
-            val classInfo = buildBundleForClassInfo(classId,className)
-            navController.navigate(R.id.signDataForEachClass,classInfo)
+            val classInfo = buildBundleForClassInfo(classId, className)
+            navController.navigate(R.id.signDataForEachClass, classInfo)
         }
         activity?.runOnUiThread {
             classListLayout?.addView(classInfoView)
         }
     }
-    private fun dip2px(dpValue:Float): Int {
+
+    private fun dip2px(dpValue: Float): Int {
         val scale = context?.resources?.displayMetrics?.density;
         return (dpValue * scale!! + 0.5f).toInt()
     }
-    private fun px2dip(pxValue:Float):Int{
+
+    private fun px2dip(pxValue: Float): Int {
         val scale = context?.resources?.displayMetrics?.density;
         return (pxValue / scale!! + 0.5f).toInt()
     }
@@ -113,6 +125,7 @@ class SignDataForTeacher : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_data_for_teacher, container, false)
     }
+
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =

0cb76e38117bb3d7872a7acc6b3f19da49635795
a1020151695 <1020151695@qq.com>
2020-12-26 11:06:29 +0800

    tabbarUtil for teacher ok

--- Android_Qin/app/src/main/java/com/example/android_qin/TeacherActivity.kt
+++ Android_Qin/app/src/main/java/com/example/android_qin/TeacherActivity.kt
@@ -3,7 +3,9 @@ package com.example.android_qin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.android_qin.util.TabBarUtil
 import com.xuexiang.xui.widget.tabbar.TabSegment
 import org.json.JSONArray
 
@@ -15,41 +17,39 @@ class TeacherActivity : AppCompatActivity() {
         configTabBar()
     }
 
-    private fun configTabBar(){
-        var tabSegment=findViewById<TabSegment>(R.id.teacher_tab_bar)
-        tabSegment.mode = TabSegment.MODE_FIXED
-        val locationTab = TabSegment.Tab(this.applicationContext.getDrawable(R.drawable.location),this.applicationContext.getDrawable(R.drawable.location_chosen),"打卡",true)
-        val dataTab = TabSegment.Tab(this.applicationContext.getDrawable(R.drawable.data),this.applicationContext.getDrawable(R.drawable.data_chosen),"统计",true)
-        val mineTab = TabSegment.Tab(this.applicationContext.getDrawable(R.drawable.mine),this.applicationContext.getDrawable(R.drawable.mine),"我的",true)
-        tabSegment.addTab(locationTab)
-        tabSegment.addTab(dataTab)
-        tabSegment.addTab(mineTab)
-        tabSegment.selectTab(0)
-        tabSegment.notifyDataChanged()
+    private fun configTabBar() {
+        var tabSegment = findViewById<TabSegment>(R.id.teacher_tab_bar)
         val teacherInfo = buildBundleForTeacher()
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_for_teacher) as NavHostFragment
-        val navController = navHostFragment.navController
-        tabSegment.setOnTabClickListener {
-            when(it){
-                0 -> navController.navigate(R.id.locationFragmentForTeacher,teacherInfo)
-                1 -> navController.navigate(R.id.signDataForTeacher,teacherInfo)
-                2 -> navController.navigate(R.id.mineForTeacher,teacherInfo)
-            }
-        }
-        navController.navigate(R.id.locationFragmentForTeacher,teacherInfo)
+        buildNavHost()
+        TabBarUtil.configTabBar(tabSegment, navController, teacherInfo, TabBarUtil.TEACHER)
+        toDefaultPage(teacherInfo)
     }
-    private fun buildBundleForTeacher():Bundle{
+
+    private fun toDefaultPage(teacherInfo: Bundle) {
+        navController?.navigate(R.id.locationFragmentForTeacher, teacherInfo)
+    }
+
+    private fun buildNavHost() {
+        if (navHostFragment == null) {
+            navHostFragment =
+                supportFragmentManager?.findFragmentById(R.id.nav_host_for_teacher) as NavHostFragment
+            navController = navHostFragment?.navController
+        }
+    }
+
+    private fun buildBundleForTeacher(): Bundle {
         val bundle = Bundle()
-        bundle.putString("teacherId",teacherId)
-        bundle.putString("teacherName",teacherName)
-        bundle.putString("classesInfoString",classesInfoString)
+        bundle.putString("teacherId", teacherId)
+        bundle.putString("teacherName", teacherName)
+        bundle.putString("classesInfoString", classesInfoString)
         return bundle
     }
+
     override fun onBackPressed() {
         //invalid back button
     }
 
-    private fun buildDataForBundle(){
+    private fun buildDataForBundle() {
         teacherId = intent.getStringExtra("teacherId").toString()
         teacherName = intent.getStringExtra("teacherName").toString()
         classesInfoString = intent.getStringExtra("classesInfo").toString()
@@ -59,5 +59,7 @@ class TeacherActivity : AppCompatActivity() {
     var teacherId = ""
     var teacherName = ""
     var classesInfoString = ""
-    var classesInfo:JSONArray? = null
+    var classesInfo: JSONArray? = null
+    var navHostFragment: NavHostFragment? = null
+    var navController: NavController? = null
 }
